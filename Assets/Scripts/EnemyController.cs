a74ad6b6a4c7b06ea5ce59d6072b59607ce95386
denchu <denchu4491@gmail.com>
2018-08-30 00:37:57 +0900

    Enemyがターゲットに移動するように実装
    NavMesh強すぎるよね

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour
+{
+
+    private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform targetTrfm;
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate()
+    {
+        navMeshAgent.SetDestination(targetTrfm.position);
+    }
+}
