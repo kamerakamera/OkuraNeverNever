f8bd28f85125ba43b1f95d5ed57520f27152c59b
denchu <denchu4491@gmail.com>
2018-08-30 16:42:43 +0900

    Enemyの停止解除処理の実装

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -44,4 +44,9 @@ public class EnemyController : MonoBehaviour
     {
         navMeshAgent.isStopped = true;
     }
+
+    public void Restart()
+    {
+        navMeshAgent.isStopped = false;
+    }
 }
